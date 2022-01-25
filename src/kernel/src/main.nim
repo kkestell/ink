98b620b25d0482511c632a38eaf446b04013965c
Kyle Kestell <kyle@kestell.org>
2022-01-25 13:30:43 -0600

    test

--- src/kernel/src/main.nim
+++ src/kernel/src/main.nim
@@ -40,13 +40,13 @@ const
   UART_PORT_COM1* = 0x3f8'u16
 
 proc uart_init*() =
-  outb(UART_PORT_COM1 + 1, char(0x00)) ##  Disable all interrupts
-  outb(UART_PORT_COM1 + 3, char(0x80)) ##  Enable DLAB (set baud rate divisor)
-  outb(UART_PORT_COM1 + 0, char(0x03)) ##  Set divisor to 3 (lo byte) 38400 baud
-  outb(UART_PORT_COM1 + 1, char(0x00)) ##                   (hi byte)
-  outb(UART_PORT_COM1 + 3, char(0x03)) ##  8 bits, no parity, one stop bit
-  outb(UART_PORT_COM1 + 2, char(0xC7)) ##  Enable FIFO, clear them, with 14-byte threshold
-  outb(UART_PORT_COM1 + 4, char(0x0B)) ##  IRQs enabled, RTS/DSR set
+  outb(UART_PORT_COM1 + 1, char(0x00)) #  Disable all interrupts
+  outb(UART_PORT_COM1 + 3, char(0x80)) #  Enable DLAB (set baud rate divisor)
+  outb(UART_PORT_COM1 + 0, char(0x03)) #  Set divisor to 3 (lo byte) 38400 baud
+  outb(UART_PORT_COM1 + 1, char(0x00)) #                   (hi byte)
+  outb(UART_PORT_COM1 + 3, char(0x03)) #  8 bits, no parity, one stop bit
+  outb(UART_PORT_COM1 + 2, char(0xC7)) #  Enable FIFO, clear them, with 14-byte threshold
+  outb(UART_PORT_COM1 + 4, char(0x0B)) #  IRQs enabled, RTS/DSR set
 
 proc uart_is_transmit_buffer_empty*(): bool =
   return (int(inb(UART_PORT_COM1 + 5)) and 0x20) != 0
